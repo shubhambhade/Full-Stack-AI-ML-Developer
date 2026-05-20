5294ff7ddf1ddb4a1114125550eb7180e9f4d38c
shubham <shubhambhade5023@gmail.com>
2026-05-20 07:52:44 +0530

    js variables

---
+++ frontend/javascript/1_variable.js
@@ -0,0 +1,42 @@
+// Declaration
+let name;
+// initialization
+name = "sham";
+// use
+console.log(name);
+
+let age;
+age = 24;
+console.log(age);
+
+//  declaration + initialization
+let programming_name = "javascript";
+console.log(programming_name);
+
+let a = 10;
+let b = 20;
+let c = 30;
+console.log(a, b, c);
+
+let x = 10,
+  y = 20,
+  z = 30;
+console.log(x, y, z);
+
+// Suppose price of two products comes from database, you need to store these prices and show the total as output.
+let price1 = 499;
+let price2 = 299;
+let totalPrice = price1 + price2;
+console.log("Total Price : ", totalPrice);
+
+// A user enters their first and last name into a form, and you need to display the full name.
+
+let firstName = "sham";
+let lastName = "sharma";
+let fullName = firstName + " " + lastName; // concatination
+console.log("Full Name : ", fullName);
+
+console.log(typeof firstName);
+
+console.log("2" + 2); 
+console.log("2" - 2);
